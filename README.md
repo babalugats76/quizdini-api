4b721c63c2dc38fcf1c2659bba4cc135df070151
James Colestock <james@colestock.com>
2019-03-24 15:12:28 -0600

    Dummied routes in order to test front end

---
+++ README.md
@@ -0,0 +1,11 @@
+## Google OAuth
+
+## Passport
+
+- Website for [PassportJS](http://www.passportjs.org/) middleware that we will be using to streamline authentication (using various strategies)
+- Install via `npm install passport`
+
+### Google OAuth Strategy
+
+- Install the strategy via `npm install passport-google-oauth20@2 --save`; this is required because of fixes made to account for the deprecation of the Google+ service
+- Using OAuth requires a project be setup, etc., using the [Google Developer's Console](https://console.developers.google.com)
--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 const express = require('express');
-
+const cors = require('cors');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+app.use(cors());
+
+app.get('/match/4', (req, res) => {
+  console.log('received request...', req);
+  res.json({
+    title: 'Basic Computer Science Vocabulary',
+    topic: 'Programming',
+    author: 'Mr. James Colestock',
+    instructions: 'Take a moment to review key programming terms from Unit 1',
+    config: {
+      itemsPerBoard: 9,
+      duration: 50
+    },
+    matches: ''
+  });
 });
+
+/* Use this redirect in lieu of the wildcard: http://localhost:5000/auth/google/callback */
+
+// ClientID: 624001032721-0eeueit2htuk4fecq8h1vcvto2v573rd.apps.googleusercontent.com
+// Client Secret: dxMcCjysrX46A79cLnkV_slq
+//passport.use(new GoogleStrategy({}));
 
 const PORT = process.env.port || 5000;
 app.listen(PORT);
